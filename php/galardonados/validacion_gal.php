1a0cd8e7d1a8ce3d4182f2bd8a0990a8cfacad45
Oscar David Romero <oromeroh1700@alumno.ipn.mx>
2023-01-18 10:56:13 -0600

    v1.8.5 Validacion de una unica vez la asistencia en galardonados

--- php/galardonados/validacion_gal.php
+++ php/galardonados/validacion_gal.php
@@ -4,6 +4,8 @@ session_start();
 include("../sinPagina/configDB.php");
 
     $id = $_SESSION["usuarioID"];
+
+
     $sql = "SELECT * FROM galardonado WHERE idGalardonado = '$id'";
     $res = mysqli_query($conexion, $sql);
     $galardonado = mysqli_fetch_array($res);
@@ -12,6 +14,16 @@ include("../sinPagina/configDB.php");
 if(isset($_SESSION["usuarioID"])){
 ?>
         <!-- **Section** -->
+        <?php
+                // Verificar si se ha registrado...
+                $sql = "SELECT * FROM asistencia WHERE idGalardonado = '$id'";
+                $res = mysqli_query($conexion, $sql);
+                $asistencia = mysqli_fetch_array($res);
+                if($asistencia != 0){
+                    echo "<h1>YA SE HA VALIDADO LA ASISTENCIA</h1>
+                        <p> Si necesita cambiar modificar algun dato, acude con el director de tu instituo.</p>";
+                } else{
+                ?>
                     <h1>Favor de registrar su asistencia</h1>
                     <hr>
                     <form role="form" method="POST" action="validarExito.php?id=<?php echo $id;?>">
@@ -65,6 +77,7 @@ if(isset($_SESSION["usuarioID"])){
                     </form>
                     <hr>
                     <p>La técnica al servicio de la patria</p>
+                <?php } ?>
 <?php include("pie.php");?>
 <?php
 }
